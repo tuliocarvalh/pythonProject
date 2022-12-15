ee0c747a0ee83d649621ff5943e81516cc76a3f5
tuliocarvalh <tuliocarvalh@gmail.com>
2022-12-15 13:45:55 -0300

    First codes

---
+++ cursos/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from .views import CursoAPIView, CursosAPIView, AvaliacaoAPIView, AvaliacoesAPIView
+
+urlpatterns = [
+    path('cursos/', CursosAPIView.as_view(), name='cursos'),
+    path('cursos/<int:pk>/', CursoAPIView.as_view(), name='curso'),
+    path('cursos/<int:curso_pk>/avaliacoes/', AvaliacoesAPIView.as_view(), name='curso_avaliacoes'),
+    path('cursos/<int:curso_pk>/avaliacoes/<int:avaliacao_pk>/', AvaliacoesAPIView.as_view(), name='curso_avaliacoes'),
+
+
+    path('avaliacoes/', AvaliacoesAPIView.as_view(), name='avaliacoes'),
+    path('avaliacoes/<int:pk>/', AvaliacaoAPIView.as_view(), name='avaliacao'),
+]
